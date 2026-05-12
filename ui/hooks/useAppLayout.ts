4b822cddd194a1074bc567bd4d3d214af88b55d7
Zhangjian He <hezhangjian97@gmail.com>
2026-05-12 21:27:53 +0800

    feat: add app layout hook

---
+++ ui/hooks/useAppLayout.ts
@@ -0,0 +1,32 @@
+import { useMemo, useState } from "react";
+import type { CSSProperties } from "react";
+
+export type AppLayoutState = {
+  rootStyle: CSSProperties;
+  isLeftCollapsed: boolean;
+  isRightCollapsed: boolean;
+  toggleLeftCollapsed: () => void;
+  toggleRightCollapsed: () => void;
+};
+
+export function useAppLayout(): AppLayoutState {
+  const [isLeftCollapsed, setIsLeftCollapsed] = useState(false);
+  const [isRightCollapsed, setIsRightCollapsed] = useState(false);
+
+  const rootStyle = useMemo(
+    () =>
+      ({
+        "--left-panel-width": isLeftCollapsed ? "0px" : "280px",
+        "--right-panel-width": isRightCollapsed ? "0px" : "280px",
+      }) as CSSProperties,
+    [isLeftCollapsed, isRightCollapsed],
+  );
+
+  return {
+    rootStyle,
+    isLeftCollapsed,
+    isRightCollapsed,
+    toggleLeftCollapsed: () => setIsLeftCollapsed((value) => !value),
+    toggleRightCollapsed: () => setIsRightCollapsed((value) => !value),
+  };
+}
